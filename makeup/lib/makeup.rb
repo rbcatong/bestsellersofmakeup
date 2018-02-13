b6c22cf76b980620681b74588dd8b820429e7fc6
Rebecca Tong <rtong@buffalo.edu>
2018-02-13 22:35:06 +0000

    added require relative in makeup.rb

--- makeup/lib/makeup.rb
+++ makeup/lib/makeup.rb
@@ -1,5 +1,9 @@
 require "makeup/version"
 
-module Makeup
-  # Your code goes here...
-end
+require 'pry'
+
+require_relative './makeup/version'
+require_relative './makeup/makeupcollection.rb'
+require_relative './makeup/skincarecollection.rb'
+require_relative './makeup/cli.rb'
+require_relative './makeup/scraper.rb'
