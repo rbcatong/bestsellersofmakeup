429f9f5280fa2b43a61bac722eaefcb6fb586bad
Rebecca Tong <rtong@buffalo.edu>
2018-02-13 22:35:42 +0000

    added skincarecollection class and initialize method

---
+++ makeup/lib/makeup/skincarecollection.rb
@@ -0,0 +1,12 @@
+class Makeup::Skincarecollection
+  attr_accessor :name, :description, :price, :url
+
+
+  def initialize(name,description,price,url)
+    @name = name
+    @description = description
+    @price = price
+    @url = url
+  end
+
+end
